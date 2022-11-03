4a8842ab206561c273da8b6f2f88864c240f2102
Rilazy <35211613+Rilazy@users.noreply.github.com>
2022-11-03 02:38:45 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,8 +1,8 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "ProgCalc"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "ProgCalc"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
